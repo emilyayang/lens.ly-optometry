44cbcb05e6e82ec1209faf50b63122fe0c4bf64b
Emily Yang <emilyyang@emilys-air.lan>
2019-11-26 23:20:56 -0800

    added main component

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,28 @@
-const path = require('path');
-var SRC_DIR = path.join(__dirname, '/client/src');
-var DIST_DIR = path.join(__dirname, '/client/dist');
-
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 module.exports = {
-  entry: `${SRC_DIR}/index.js`,
-  output: {
-    path: DIST_DIR,
-    filename: 'bundle.js',
-  },
   module: {
     rules: [
       {
-        test: /\.jsx?/,
-        include: SRC_DIR,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015']
-        },
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
       },
       {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ]
 };
