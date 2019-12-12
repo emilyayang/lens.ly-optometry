38c26d4a4a4c616d1a51d58a7521e0ad33bdca34
Emily Yang <emilyayang@gmail.com>
2019-12-11 22:55:12 -0600

    created store

---
+++ public/src/Store.js
@@ -0,0 +1,30 @@
+import { createStore, compose } from 'redux';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { browserHistory } from 'react-router';
+
+//import root reducer
+import rootReducer from './reducers/index'
+
+//create an obj for the default data
+const defaultState = {
+  orders: [],
+  order: {},
+  savedRX: []
+};
+
+const enhancers = compose(
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+);
+
+const store = createStore(rootReducer, defaultState, enhancers);
+export const history = syncHistoryWithStore(browserHistory, store);
+
+//to hot reload reducers w/ webpack
+if (module.hot) {
+  module.hot.accept('./reducers/', () => {
+    const nextRootReducer = require('./reducers/index').default;
+    store.replaceReducer(nextRootReducer);
+  });
+}
+
+export default store;
