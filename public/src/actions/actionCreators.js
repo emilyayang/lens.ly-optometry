65dac6559b2bf7cbea50f67c72c6652d21f53da9
Emily Yang <emilyayang@gmail.com>
2019-12-12 21:36:17 -0600

    added action creators

---
+++ public/src/actions/actionCreators.js
@@ -0,0 +1,34 @@
+//actions get dispatched but doesn't update state in store need reducer, every reducer will run at every dispatch
+export function addOrder(userId, OD, OS, PD, RXname, add) {
+  return {
+    type: "ADD_ORDER",
+    //these 5 things are payload
+    userId,
+    OD,
+    OS,
+    PD,
+    RXname,
+    add
+  }
+}
+
+export function addRX(userId, OD, OS, PD, RXname, add) {
+  return {
+    type: "SAVE_RX",
+    userId,
+    OD,
+    OS,
+    PD,
+    RXname,
+    add
+  }
+}
+
+export function removeRX(userId, RXname, i) {
+  return {
+    type: "REMOVE_RX",
+    userId,
+    RXname,
+    i
+  }
+}
