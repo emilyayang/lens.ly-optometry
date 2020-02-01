069378f8a5c7a59a18404f9247eea7475c652ebb
Emily Yang <emilyayang@gmail.com>
2020-01-31 19:31:56 -0800

    created auth actions

---
+++ public/src/actions/actionTypes.js
@@ -0,0 +1,15 @@
+export const BEGIN_API_CALL = "BEGIN_API_CALL";
+export const API_CALL_ERROR = "API_CALL_ERROR";
+
+export const SIGNUP_SUCCESS = "SIGNUP_SUCCESS";
+export const SIGNUP_ERROR = "SIGNUP_ERROR";
+
+export const SIGNIN_SUCCESS = "SIGNIN_SUCCESS";
+export const SIGNIN_ERROR = "SIGNIN_ERROR";
+export const EMAIL_NOT_VERIFIED = "EMAIL_NOT_VERIFIED";
+
+export const SIGNOUT_SUCCESS = "SIGNOUT_SUCCESS";
+export const SIGNOUT_ERROR = "SIGNOUT_ERROR";
+
+export const RESET_SUCCESS = "RESET_SUCCESS";
+export const RESET_ERROR = "RESET_ERROR";
