f46ce828dd55e3cf5116c034b26b55d516e88cba
Emily Yang <emilyayang@gmail.com>
2020-03-07 00:05:20 -0800

    added post to RX

--- public/src/actions/rx.js
+++ public/src/actions/rx.js
@@ -1,58 +1,60 @@
 import axios from "axios";
 
-export function getRX(){
+export function getRXs(){
     return(dispatch)=>{
-        return axios.get("http://localhost:5001/robertson-optometry/us-central1/api/").then((response)=>{
+        return axios.get("http://localhost:5001/robertson-optometry/us-central1/api/RXs").then((response)=>{
             dispatch(loadRX(response.data));
         })
     }
 }
 
-export function loadRX(RX){
-  console.log(RX)
+export function setRXs(RX){
     return{
-        type:"LOAD_RX",
-        RX
+        type:"SET_RXS",
+        RXs
     }
 }
 
-export function postRX(){
-  return(dispatch)=>{
-      return axios.get("http://localhost:5001/robertson-optometry/us-central1/api/").then((response)=>{
-          dispatch(loadRX(response.data));
-      })
-  }
-}
 
-export function setRXState(ODSPH, ODCLY, ODAXIS, OSSPH, OSCLY, OSAXIS, PD, ODPD, OSPD, ODadd, OSadd, ODPrismHor, ODPrismHorDirection, ODPrismVert, ODPrismVertDirection, OSPrismHor, OSPrismHorDirection, OSPrismVert, OSPrismVertDirection, PrismOD, PrismOS, RXname) {
+export function postRX({ ODSPH, ODCLY, ODAXIS, OSSPH, OSCLY, OSAXIS, PD, ODPD, OSPD, ODadd, OSadd, ODPrismHor, ODPrismHorDirection, ODPrismVert, ODPrismVertDirection, OSPrismHor, OSPrismHorDirection, OSPrismVert, OSPrismVertDirection, PrismOD, PrismOS, RXname }) {
+  return (dispatch) => {
+    return axios.post("http://localhost:5001/robertson-optometry/us-central1/api/RXs", { ODSPH, ODCLY, ODAXIS, OSSPH, OSCLY, OSAXIS, PD, ODPD, OSPD, ODadd, OSadd, ODPrismHor, ODPrismHorDirection, ODPrismVert, ODPrismVertDirection, OSPrismHor, OSPrismHorDirection, OSPrismVert, OSPrismVertDirection, PrismOD, PrismOS, RXname })
+      .then(response => {
+        dispatch(saveRX(response.data))
+      })
+      .catch(error => {
+        console.log("error posting order", error);
+      });
+  };
+};
+
+export function saveRX(data) {
   return {
     type: "SAVE_RX",
-    ODSPH,
-    ODCLY,
-    ODAXIS,
-    OSSPH,
-    OSCLY,
-    OSAXIS,
-    PD,
-    ODPD,
-    OSPD,
-    ODadd,
-    OSadd,
-    ODPrismHor,
-    ODPrismHorDirection,
-    ODPrismVert,
-    ODPrismVertDirection,
-    OSPrismHor,
-    OSPrismHorDirection,
-    OSPrismVert,
-    OSPrismVertDirection,
-    PrismOD,
-    PrismOS,
-    RXname
+    ODSPH: data.ODSPH,
+    ODCLY: data.ODCLY,
+    ODAXIS: data.ODAXIS,
+    OSSPH: data.OSSPH,
+    OSCLY: data.OSCLY,
+    OSAXIS: data.OSAXIS,
+    PD: data.PD,
+    ODPD: data.ODPD,
+    OSPD: data.OSPD,
+    ODadd: data.ODadd,
+    OSadd: data.OSadd,
+    ODPrismHor: data.ODPrismHor,
+    ODPrismHorDirection: data.ODPrismHorDirection,
+    ODPrismVert: data.ODPrismVert,
+    ODPrismVertDirection: data.ODPrismVertDirection,
+    OSPrismHor: data.OSPrismHor,
+    OSPrismHorDirection: data.OSPrismHorDirection,
+    OSPrismVert: data.OSPrismVert,
+    OSPrismVertDirection: data.OSPrismVertDirection,
+    PrismOD: data.PrismOD,
+    PrismOS: data.PrismOS,
+    RXname: data.RXname
   }
 }
-
-
 
 export function removeRX(userId, RXname, i) {
   return {
@@ -62,3 +64,4 @@ export function removeRX(userId, RXname, i) {
     i
   }
 }
+
--- public/src/reducers/RXReducer.js
+++ public/src/reducers/RXReducer.js
@@ -1,4 +1,4 @@
-// function RXReducer(state = [], action) {
+// export function RX(state = [], action) {
 //   switch (action.type) {
 //     case 'SAVE_RX':
 //       // return the new state with the new comment
@@ -22,7 +22,7 @@
 //   }
 // }
 
-// function RX(state = [], action) {
+// export function RX(state = [], action) {
 //   if (typeof action.RXname !== 'undefined') {
 //     return {
 //       // take the current state
@@ -34,4 +34,35 @@
 //   return state;
 // }
 
-// export default RXReducer;
+// export function RX(state = [], action) {
+//   switch (action.type) {
+//     case 'SAVE_RX':
+//       // return the new state with the new comment
+//       return [...state, {
+//         OD: action.OD, 
+//         OS: action.OS, 
+//         PD: action.PD,
+//         RXname: action.RXname,
+//         add: action.add
+//       }];
+//     case 'REMOVE_RX':
+//       // we need to return the new state without the deleted comment
+//       return [
+//         // from the start to the one we want to delete
+//         ...state.slice(0, action.i),
+//         // after the deleted one, to the end
+//         ...state.slice(action.i + 1)
+//       ]
+//     default:
+//       return state;
+//   }
+// }
+
+export function RXs(state = [], action) {
+  switch (action.type) {
+    case 'SET_RXS':
+      return action.RXs;
+    default:
+      return state;
+  }
+}
